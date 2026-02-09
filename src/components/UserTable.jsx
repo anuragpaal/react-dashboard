bbfa4aa8aa08a0bcd554942c180c85f8528ff23d
Anurag <anurag8731@gmail.com>
2026-02-09 12:28:54 +0530

    Day 5: Added search filter for users table with no-data-found state

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 function UserTable() {
   const [users, setUsers] = useState([]);
+  const [search,setSearch] = useState("");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -11,11 +12,26 @@ function UserTable() {
         setUsers(data);
         setLoading(false);
     });
-  },[])
+  },[]);
+
+  const filteredUsers = users.filter(user =>
+    user.name.toLowerCase().includes(search.toLowerCase()) ||
+    user.email.toLowerCase().includes(search.toLowerCase())
+  )
 
   return(
         <div className="bg-white p-5 rounded shadow mt-10">
             <h3 className="text-xl font-semibold mb-4">Users</h3>
+
+            {/* Search */}
+
+            <input 
+            type="text"
+            placeholder="Search User..."
+            className="border p-2 mb-4 w-full rounded"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            />
 
             {loading ? (
                 <p>Loading Users...</p>
@@ -31,13 +47,22 @@ function UserTable() {
                     </thead>
 
                     <tbody>
-                        {users.map(user => (
+
+                        {filteredUsers.length === 0 ? (
+                            <tr>
+                                <td colSpan="3" className="p-4 text-center text-red-500">
+                                    No data found
+                                </td>
+                            </tr>
+                        ) : (
+                             filteredUsers.map(user => (
                             <tr key={user.id} className="text-center border-t">
                                 <td className="p-2">{user.name}</td>
                                 <td className="p-2">{user.email}</td>
                                 <td className="p-2">{user.address.city}</td>
                             </tr>
-                        ))}
+                        ))
+                        )}
                     </tbody>
 
                 </table>
